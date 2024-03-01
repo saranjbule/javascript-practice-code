85aa0e7c2ac062168e74f6307ee1119012f42099
saranj <saranj@trendlyne.com>
2024-03-01 09:11:58 +0530

    update

---
+++ question.js
@@ -0,0 +1,125 @@
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+
+startButton.addEventListener("click", startInterval);
+stopButton.addEventListener("click", stopInterval);
+
+// component to show current time to user
+
+let interval = null;
+function startInterval() {
+  interval = setInterval(() => {
+    const d = new Date();
+    console.log(d.toTimeString());
+  }, 1000);
+}
+
+function stopInterval() {
+  clearInterval(interval);
+}
+
+// predict output
+// QUE: 1
+console.log(1);
+function foo() {
+  console.log(2);
+  setTimeout(() => console.log("3/1"), 1000);
+  setTimeout(() => console.log("3/0"), 0);
+  console.log(4);
+}
+console.log(5);
+foo();
+console.log(6);
+
+// 1 5 2 4 6 3/0 3/1
+
+// QUE: 2
+const person = {
+  name: "saranj",
+  print: () => {
+    console.log(this.name); // undefined
+    // arrow function do not have their own this they provide reference of this with surrounding env (window)
+    // hence window do not have any name property (window.name) into it
+  },
+  print1: function () {
+    console.log(this.name); // saranj // this refer to person object
+  },
+};
+
+person.print();
+person.print1();
+
+// print 1 to 5 after 1sec each
+
+function print() {
+  let count = 0;
+  const interval = setInterval(() => {
+    if (count < 5) {
+      console.log(++count);
+    } else {
+      clearInterval(interval);
+    }
+  }, 1000);
+}
+
+print();
+
+// Everything in javascript is an object
+
+function x(para) {
+  const n = "saranj";
+  const age = 24;
+  console.log(n, para);
+}
+
+x("bule"); // saranj bule
+console.log(x); // f
+console.log(x.name); // x // build-in method get the name of function
+console.log(x.age); // undefined
+
+// function have predefined properties - name, length, prototype ...
+x.category = "xx"; // attaching a user defined property (category) to the JavaScript object (i.e., function)
+console.log(x.category); // xx
+
+x.name = "newName"; // do not alter in-build function
+console.log(x.name); // x
+
+x.category = "yy";
+console.log(x.category); // yy
+
+console.log(x.prototype); // constructor object
+
+// The Object.create() method creates a new object using the prototype of the given object.
+
+const p1 = {
+  name: "p1",
+  getName: function () {
+    console.log(this.name);
+  },
+};
+
+console.log(p1);
+console.log(p1.name); // p1
+// creating object using prototype of p1
+const p2 = Object.create(p1);
+
+console.log(p2);
+console.log(p1.name); // p1
+
+// predict output
+function createUser(name) {
+  const userObj = Object.create(userFunctionStore); 
+  userObj.name = name;
+  return userObj;
+}
+
+const userFunctionStore = {
+  greet: function() {
+      console.log(`Hello ${this.name}`)
+  },
+}
+
+const user1 = createUser("Foo")
+console.log(user1) //{name: 'Foo'}
+console.log(user1.name) // Foo
+user1.greet() // Hello Foo
