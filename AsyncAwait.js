f46382dd93307fc4ad72352c6dc7dec6a84bf60e
saranj <saranj@trendlyne.com>
2024-03-09 15:26:56 +0530

    update

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -18,7 +18,9 @@ console.log(asyncFun()); // Promise
 console.log(asyncFun1()); // Promise
 
 const promise = new Promise((resolve, reject) => {
-  resolve("Resolved Promise");
+  setTimeout(() => {
+    resolve("Resolved Promise");
+  }, 5000);
 });
 
 // old way
@@ -28,18 +30,91 @@ function handlePromiseOld() {
 }
 
 handlePromiseOld();
+// in old
 // Resolved Promise
 
 // new way
 async function handlePromiseNew() {
+  console.log("hello");
   const result = await promise;
   console.log(result);
   console.log("in new");
 }
 
-// function execution will stop till promise is resolved 
+handlePromiseNew();
+// hello # quickly
+// Resolved Promise # after promise resolved
+// in new # after promise resolved
+
+// async function execution will stop/ suspended till promise is resolved (i.e., call stack gets empty)
+// JS engine will wait for promise to get resolved and hold the function execution at await
 // i.e., function execution context is pop out of call stack
 // once resolved function continues from where it was left off
 
-handlePromiseNew();
-// Resolved Promise
+const p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("i am done");
+  }, 10000);
+});
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("i am done 2");
+  }, 5000);
+});
+
+async function handlePromiseNew1() {
+  console.log("hello");
+  const result = await p;
+  console.log(result);
+
+  const result1 = await p;
+  console.log(result1);
+}
+
+handlePromiseNew1();
+
+// hello # quickly
+// i am done # after 10sec
+// i am done # with above msg after 10sec
+
+async function handlePromiseNew1() {
+  const result = await p;
+  console.log(result);
+
+  const result1 = await p1;
+  console.log(result1);
+}
+
+// i am done # after 10sec
+// i am done 2 # just after that
+
+async function handlePromiseNew1() {
+  const result = await p1;
+  console.log(result);
+
+  const result1 = await p;
+  console.log(result1);
+}
+
+// i am done 2 # after 5sec
+// i am done # after 5sec, total 10sec after
+
+const API = "https://api.github.com/users/saranjbule";
+
+async function getData() {
+  const respose = await fetch(API);
+  console.log(respose)
+  const data = await respose.json();
+  console.log(data);
+}
+
+getData();
+// without await at line 108 promise
+// with await at line 108 json data
+
+/**
+ * handling erros
+ * in promises - by attaching failure (.catch()) callbacks
+ * in async await - by using try catch block or by attaching failure callback to aysnc function
+ */
--- PromiseAPI.js
+++ PromiseAPI.js
@@ -3,19 +3,20 @@
  * - to make multiple parallel API calls and get the result
  * - to handle multple Promises together
  * - take array of promises (iterable) as input
- * - fail fast technique
+ * - fail fast technique | ALL or NONE
  */
-
-const [r1, r2, r3] = Promise.all([p1, p2, p3]);
 
 // r1, r2, r3 are result of promises p1, p2, p3
 // p1 -> r1 (5s), p2 -> r2 (2s), p3 -> r3 (8s)
+
 // to get the final result array total time will be 8s max(5s, 2s, 8s)
-// as Promise.all() wait for all of them to finish
+// as Promise.all() wait for all of them to resolved
 
 // if any one of the promise gets rejected Promise.all() will throws the error of that promise instantly
-// it will not wait for other promises to get executed
+// it will not wait for other promises to get their result (settle [resolved or rejected])
+
 // whenever the promises are executed we can not terminate the promise in between, once initiated
+// cancelable promises
 
 /**
  * Promise.allSettled()
@@ -36,6 +37,43 @@ const [r1, r2, r3] = Promise.all([p1, p2, p3]);
 /**
  * Promise.any()
  * - Similar to Promise.race() but seeking for first success
- * - First one to ressolved first get return to the result set
+ * - First one to ressolved first gets return to the result set
  * - if every promise get rejected then result will be AGGREGATE ERROR i.e., [err1, err2, err3] (err.errors)
  */
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve("fullfiled P1");
+    reject("Rejected P1");
+  }, 1000);
+});
+
+const p2 = new Promise((resolved, reject) => {
+  setTimeout(() => {
+    // resolved("fullfiled P2");
+    reject("Rejected P2");
+  }, 3000);
+});
+
+const p3 = new Promise((resolved, reject) => {
+  setTimeout(() => {
+    // resolved("fullfiled P3");
+    reject("Rejected P3");
+  }, 5000);
+});
+
+Promise.all([p1, p2, p3])
+  .then((p) => console.log(p))
+  .catch((p) => console.log(p));
+
+Promise.allSettled([p1, p2, p3])
+  .then((p) => console.log(p))
+  .catch((p) => console.log(p));
+
+Promise.race([p1, p2, p3])
+  .then((p) => console.log(p))
+  .catch((p) => console.log(p));
+
+Promise.any([p1, p2, p3])
+  .then((p) => console.log(p))
+  .catch((p) => console.log(p));
